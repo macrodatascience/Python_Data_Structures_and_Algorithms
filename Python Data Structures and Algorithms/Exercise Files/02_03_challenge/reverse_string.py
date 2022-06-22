3e6781cd043e0c7f6020e3ed2e8725c0e48e62cb
navee <macrodatascience@gmail.com>
2022-06-22 10:04:01 +0100

    Reverse a string using the Stack data structure

---
+++ Python Data Structures and Algorithms/Exercise Files/02_03_challenge/reverse_string.py
@@ -0,0 +1,21 @@
+"""
+Python Data Structures - A Game-Based Approach
+Stack challenge
+Robin Andrews - https://compucademy.net/
+"""
+
+import stack
+
+string = "gninraeL nIdekniL htiw tol a nraeL"
+reversed_string = ""
+s = stack.Stack()
+
+for each in string:
+    s.push(each)
+
+while not s.is_empty():
+    reversed_string+=s.pop()
+# Your solution here.
+
+print(reversed_string)
+print(string[::-1])
